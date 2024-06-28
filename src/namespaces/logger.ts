2d774c9b731c8c0a279a70a05e15fc5a2d681e36
raphdcst <rdecostaire@gmail.com>
2024-06-28 20:27:16 +0200

    Modified utils namespaces and added custom logger

--- src/namespaces/logger.ts
+++ src/namespaces/logger.ts
@@ -3,4 +3,6 @@ import { Logger } from "@ghom/logger";
 export const fetchLogger = new Logger({section: "fetch"})
 export const createLogger = new Logger({section: "create"})
 export const deleteLogger = new Logger({section: "delete"})
+export const sendLogger = new Logger({section: "send"})
+
 
--- src/namespaces/utils.ts
+++ src/namespaces/utils.ts
@@ -17,19 +17,19 @@ export async function checkChannel(message: Message<true>, name: string): Promis
                     name: name,
                     type: ChannelType.GuildText
                 })
-                .catch((e) => {
+                    .catch((e) => {
 
-                    const err = e as app.DiscordAPIError
+                        const err = e as app.DiscordAPIError
 
-                    return app.createLogger.error(`${err.name} Failed to create "${name}" in "${message.guild}" (${message.guild.id})`)
-                })
+                        return app.createLogger.error(`${err.name} Failed to create "${name}" in "${message.guild}" (${message.guild.id})`)
+                    })
 
                 app.createLogger.success(`Created "${name}" in "${message.guild}" (${message.guild.id})`)
 
                 return channel
             }
 
-            else if (exists) {
+            else {
 
                 app.fetchLogger.success(`Channel "${name}" already exists, returning channel...`)
 
@@ -40,9 +40,42 @@ export async function checkChannel(message: Message<true>, name: string): Promis
 
             const err = e as app.DiscordAPIError
 
-            app.fetchLogger.error(`${err.name} Failed to fetch channels from "${message.guild}" (${message.guild.id})`)
+            return app.fetchLogger.error(`${err.name} Failed to fetch channels from "${message.guild}" (${message.guild.id})`)
         })
 
 
     return channels
-  }
+}
+
+export async function postArticles(message: Message<true>, items: app.Item[]) {
+
+    const parsedName = parseName(items[0].user?.login);
+
+    const channel = await checkChannel(message, parsedName);
+
+    if (!channel) {
+        const err = new Error('No channel provided !')
+
+        return app.sendLogger.error(err)
+    }
+
+    const embeds = Object.values(items).forEach(async (value) => {
+
+        const embed = app.createItemEmbed({ item: value })
+
+        await channel.send({ embeds: [embed] })
+            .then(() => {
+
+                return app.sendLogger.success(`Found "${value.title}" in ${value.user.login} and sent it in "${channel.name}"`)
+
+            })
+            .catch((e) => {
+
+                const err = e as app.DiscordAPIError
+
+                return app.sendLogger.error(`${err.name} Failed to send "${value.title}" in ${channel.name}`)
+
+            })
+
+    })
+}
